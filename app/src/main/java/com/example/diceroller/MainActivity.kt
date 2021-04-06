10ab0a0999d9b32d9c11dfc2e665d1030e753c3d
DemonazGH <6583865@gmail.com>
2021-04-07 00:35:57 +0300

    add dices' images and made necessary changes in the code

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 /**
@@ -13,33 +14,53 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val rollButton = findViewById<Button>(R.id.button)
         rollButton.setOnClickListener {
             rollDice()
             rollDice2()
         }
+        rollDice()
+        rollDice2()
     }
 
     private fun rollDice2() {
         val dice = Dice(6)
-        dice.roll()
-        // updates screen with the result
-        val resultTextView = findViewById<TextView>(R.id.textView2)
-        resultTextView.text = dice.roll().toString()
+        val diceRoll = dice.roll()
+        //find the ImageView in the layout
+        val diceImage = findViewById<ImageView>(R.id.imageView2)
+        // determine what drawable resource to use based on the dice roll
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
     }
-
 
     /**
      * roll the dice and update screen with the result
      */
     private fun rollDice() {
-        // create new dice object
+        // create new dice object with 6 sides and roll the dice
         val dice = Dice(6)
-        dice.roll()
-        // updates screen with the result
-        val resultTextView = findViewById<TextView>(R.id.textView)
-        resultTextView.text = dice.roll().toString()
+        val diceRoll = dice.roll()
+        //find the ImageView in the layout
+        val diceImage = findViewById<ImageView>(R.id.imageView)
+        // determine what drawable resource to use based on the dice roll
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
